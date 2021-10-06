cb395351499b1e163b965c52f882a5d14d4934b7
Baturalp KIZILTAN <baturalp.kiziltan+gitlab@outlook.com>
2021-10-06 16:34:56 +0300

    initial commit

---
+++ client.c
@@ -0,0 +1,12 @@
+//
+// client.c
+//
+// Created by Baturalp KIZILTAN on 6.10.2021.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("CLIENT..!");
+}
---
+++ server.c
@@ -0,0 +1,12 @@
+//
+// server.c
+//
+// Created by Baturalp KIZILTAN on 6.10.2021.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("SERVER..!");
+}
